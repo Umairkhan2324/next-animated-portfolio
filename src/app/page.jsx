1d4df08e3dfb232602c7c2fd3795454744a686b5
umiar khan <150588169+Umairkhan2324@users.noreply.github.com>
2024-04-04 08:05:32 +0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,11 @@ const Homepage = () => {
   return   ( 
   <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
     {/*img */}
-   <div className="h-1/2 lg:h-full lg:w-1/2 position-relative">
+   <div className="h-1/2 lg:h-full lg:w-1/2 relative">
       <Image src="/hero.png" alt="me" layout="fill" className="object-contain"/>
    </div>
    {/*body*/}
-   <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col  p-5 gap-8 items-center justify-center">
+   <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
        <h1 className="text-4xl md:text-6xl font-bold mb-4"> 
         Designing Tomorrows Web Today: Welcome to My Frontend Showcase
        </h1>
